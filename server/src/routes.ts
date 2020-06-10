b1d6b82f5be9431ba85d33aa935f363aaf3b59c9
Wilson Guimarães <wilsonfalai@gmail.com>
2020-06-10 17:05:16 -0300

    validação do backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import {celebrate, Joi} from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -16,9 +18,27 @@ const itemsController = new ItemsController();
 //Request Query - Usado para filtros, paginação
 
 routes.get('/items', itemsController.index);
-routes.post('/points', upload.single('image') ,pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
+routes.post(
+    '/points', 
+    upload.single('image') ,
+    celebrate({
+        body:  Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()//regex()para definir apenas numeros e virgulas
+        })
+    },{
+        abortEarly: false // valida todos os erros de uma vez
+    }),
+    pointsController.create
+);
 
 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import {errors} from 'celebrate';
 
 const app = express();
 app.use(cors());//Dessa forma permite que todas as url acessem. Em produção é necessário definições
 app.use(express.json());
 app.use(routes);
 app.use('/uploads',express.static(path.resolve(__dirname, '..','uploads')));
+
+app.use(errors());
 
 app.listen(3333);
